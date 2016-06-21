e4db2b81f2938e46c14837d84d862232ef7f03aa
Matt Zeunert <matt@mostlystatic.com>
2016-06-21 18:51:28 +0100

    Make another step in resolution of char work.

--- backbone-todomvc/test.js
+++ backbone-todomvc/test.js
@@ -1,7 +1,10 @@
 function onload(){
     var el = document.createElement("div")
     var p = document.createElement("p")
+    var span = document.createElement("span")
+    span.innerHTML ="xxxxxxxx"
     el.appendChild(p)
+    el.appendChild(span)
     p.innerHTML = "ooo"
     document.body.appendChild(el)
 }
