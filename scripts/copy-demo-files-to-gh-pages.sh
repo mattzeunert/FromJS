4f7e2b5c0a014f4ca1dc5eb3073a6f26055a8527
Matt Zeunert <matt@mostlystatic.com>
2016-07-07 13:17:37 +0100

    Use pre-compiled from.js file.

--- scripts/copy-demo-files-to-gh-pages.sh
+++ scripts/copy-demo-files-to-gh-pages.sh
@@ -1,4 +1,4 @@
 npm run build
 cp fromjs.css gh-pages/todomvc/fromjs.css
-cp dist/from.js gh-pages/todomvc/fromjs.js
+cp dist/from.js gh-pages/todomvc/from.js
 echo "Still need to manually generate and update data.json file"
