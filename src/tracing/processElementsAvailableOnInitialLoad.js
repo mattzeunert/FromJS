939a00945a562318727c21ecdb49b8d9e32ac8fc
Matt Zeunert <matt@mostlystatic.com>
2016-07-17 13:41:45 +0100

    Remove old code.

--- src/tracing/processElementsAvailableOnInitialLoad.js
+++ src/tracing/processElementsAvailableOnInitialLoad.js
@@ -32,47 +32,4 @@ export default function processElementsAvailableOnInitialLoad(){
     mapInnerHTMLAssignment(document.body, originalHtml, "Initial Body HTML",  bodyContentAndAfter.length - originalHtml.value.length)
 
     initialHTMLHasBeenProcessed = true;
-    //processNode(document.body, originalHtml)
 }
-//
-// function processNode(el, originalHtml){
-//     var isTextNode = el.innerHTML === undefined
-//     if (isTextNode) {
-//         addElOrigin(el, "textValue", {
-//             "action": "initial html",
-//             inputValues: [stringTrace(originalHtml)],
-//             value: el.textContent,
-//             inputValuesCharacterIndex: [0]
-//         })
-//
-//     } else {
-//         var children = Array.prototype.slice.apply(el.childNodes, [])
-//
-//         addElOrigin(el, "replaceContents", {
-//             action: "initial html",
-//             children: children
-//         })
-//
-//         addElOrigin(el, "tagName", {
-//             action: "initial html",
-//             value: el.tagName,
-//             inputValues: [stringTrace(originalHtml)],
-//             inputValuesCharacterIndex: [0]
-//         })
-//
-//         for (var i = 0;i<el.attributes.length;i++) {
-//             var attr = el.attributes[i]
-//             addElOrigin(el, "attribute_" + attr.name, {
-//                 action: "initial html",
-//                 value: " " + attr.name + "='" + attr.textContent + "'",
-//                 inputValues: [stringTrace(originalHtml)],
-//                 inputValuesCharacterIndex: [0]
-//             })
-//         }
-//
-//
-//         children.forEach(function(child){
-//             processNode(child, originalHtml)
-//         })
-//     }
-// }
