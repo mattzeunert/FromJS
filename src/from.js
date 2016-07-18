e35228cde2709f9c2970f93be2e5d7db4fe46c0f
Matt Zeunert <matt@mostlystatic.com>
2016-07-18 23:05:27 +0100

    Get rid of getVisData.js

--- src/from.js
+++ src/from.js
@@ -19,3 +19,25 @@ window.saveAndSerializeDOMState = saveAndSerializeDOMState
 Object.keys(babelFunctions).forEach(function(functionName){
     window[functionName] = babelFunctions[functionName]
 })
+
+if (!window.isSerializedDomPage){
+    enableTracing()
+}
+
+import initSerializedDataPage from "../src/ui/initSerializedDataPage"
+import showFromJSSidebar from "../src/ui/showFromJSSidebar"
+
+
+setTimeout(function(){
+    if (window.isSerializedDomPage){
+        initSerializedDataPage(showFromJSSidebar);
+    } else {
+        setTimeout(function(){
+            if (window.isVis) {
+                return;
+            }
+
+            showFromJSSidebar()
+        }, 4000)
+    }
+}, 100)
