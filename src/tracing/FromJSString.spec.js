ba25b2f94dc2ebce318e375d8e8db3d712939b72
Matt Zeunert <matt@mostlystatic.com>
2016-07-18 22:21:39 +0100

    Add basic test for FromJSString.replace.

--- src/tracing/FromJSString.spec.js
+++ src/tracing/FromJSString.spec.js
@@ -1,5 +1,24 @@
 import FromJSString from "./FromJSString"
 
-describe("FromJSString", function(){
+describe("FromJSString.replace", function(){
+    it("Supports basic replace calls", function(){
+        var str = new FromJSString({
+            value: "Hello World!",
+            origin: {}
+        })
 
+        str = str.replace("Hello", "Hi");
+        expect(str.value).toBe("Hi World!");
+    })
+
+    // Should work at some point, but not working yet
+    // it("Supports submatch replacements with $n", function(){
+    //     var str = new FromJSString({
+    //         value: "Hello!",
+    //         origin: {}
+    //     })
+    //
+    //     str = str.replace(/(!)/, "$1");
+    //     expect(str.value).toBe("Hello!!")
+    // })
 })
