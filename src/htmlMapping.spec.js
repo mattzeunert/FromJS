a343ed5a2c6a7fd8304223d6f35883a0fc046c18
Matt Zeunert <matt@mostlystatic.com>
2016-07-25 10:32:16 +0100

    Add test for handling of extra space.

--- src/htmlMapping.spec.js
+++ src/htmlMapping.spec.js
@@ -37,7 +37,7 @@ fdescribe("HTML Mapping", function(){
         expect(originAndChar.origin.value[originAndChar.characterIndex]).toBe("W")
     })
 
-    it("Traces nested HTML assignments", function(done){
+    it("Traces attributes without a value correctly", function(done){
         var el = document.createElement("div")
         el.innerHTML = '<span hello>Hi</span>'
         var span = el.children[0]
@@ -56,6 +56,26 @@ fdescribe("HTML Mapping", function(){
             var characterIndex = steps[1].characterIndex
             // correctly traces back to assigned string
             expect(value).toBe("<span hello>Hi</span>")
+            expect(value[characterIndex]).toBe("H")
+            done()
+        })
+    })
+
+    it("Traces an extra space at the end of a tag correctly", function(done){
+        var el = document.createElement("div")
+        el.innerHTML = '<span >Hi</span>'
+        var span = el.children[0]
+
+        disableTracing()
+        expect(el.innerHTML).toBe('<span>Hi</span>')
+
+        // <span>[H]i</span>
+        var originAndChar = getRootOriginAtChar(span, 6);
+
+        whereDoesCharComeFrom(originAndChar.origin, originAndChar.characterIndex, function(steps){
+            var value = steps[1].originObject.value;
+            var characterIndex = steps[1].characterIndex
+            expect(value).toBe("<span >Hi</span>")
             expect(value[characterIndex]).toBe("H")
             done()
         })
