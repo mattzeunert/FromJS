f5b94faf2bcb2907f471ab456180ed7799f63507
Matt Zeunert <matt@mostlystatic.com>
2016-08-04 13:56:37 +0100

    Return source maps for chrome extension data urls.

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,20 +1,29 @@
 import processJavaScriptCode from "../src/compilation/processJavaScriptCode"
 
-console.log("cake")
+var sourceMaps = {}
 chrome.webRequest.onBeforeRequest.addListener(
   function(info){
       console.log("Intercepted: " + info.url);
       if (info.url.slice(0, "chrome-extension://".length ) === "chrome-extension://") {
         return
       }
-      if (info.url.slice(info.url.length - 3) === ".js") {
+      if (info.url.slice(info.url.length - ".js.map".length) === ".js.map") {
+        return {
+          redirectUrl: "data:," + encodeURI(sourceMaps[info.url])
+        }
+      } 
+      if (info.url.slice(info.url.length - ".js".length) === ".js") {
           var xhr = new XMLHttpRequest()
-            xhr.open('GET', info.url, false);
-            xhr.send(null);
+          xhr.open('GET', info.url, false);
+          xhr.send(null);
 
         //   var code = "console.log('sth')"
-        var code = processJavaScriptCode(xhr.responseText, {filename: info.url}).code
-          var url = "data:," + encodeURI(code)
+        var res = processJavaScriptCode(xhr.responseText, {filename: info.url})
+        var code = res.code
+        code += "\n//# sourceURL=" + info.url
+        code += "\n//# sourceMappingURL=" + info.url + ".map"
+        sourceMaps[info.url + ".map"] = JSON.stringify(res.map)
+        var url = "data:," + encodeURI(code)
         return {redirectUrl: url}
       }
   }, {urls: ["<all_urls>"]}, ["blocking"]);
