20b7d73cf011cbfa07bfac499380834228f6ca7e
Matt Zeunert <matt@mostlystatic.com>
2016-10-17 16:21:52 +0100

    Add script to automatically copy playground files to gh-pages.

---
+++ scripts/copy-files-to-playground.sh
@@ -0,0 +1,4 @@
+cp dist/from.js gh-pages/playground/fromjs/from.js
+cp dist/fromjs.css gh-pages/playground/fromjs/fromjs.css
+cp dist/inspector.js gh-pages/playground/fromjs/inspector.js
+cp dist/resolveFrameWorker.js gh-pages/playground/fromjs/resolveFrameWorker.js
