21d75baa4d784888aefe694f2da0d27dc3afae3d
Matt Zeunert <matt@mostlystatic.com>
2016-10-19 09:05:55 +0100

    Restart server on code change when running E2E tests.

--- scripts/e2e-background.sh
+++ scripts/e2e-background.sh
@@ -1,5 +1,5 @@
 trap "echo \"kill bg\";pkill -P $$" SIGTERM SIGINT
 
-node dist/server.js 9999 &
+./node_modules/.bin/nodemon dist/server.js 9999 &
 npm run ci-static-file-server &
 node src/e2e/security/sec-server.js
