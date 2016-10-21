fa40d65be928b1b616f9d1d6c779f9afa41048d0
Matt Zeunert <matt@mostlystatic.com>
2016-10-21 10:57:24 +0100

    Update React TodoMVC demo.

--- react-todomvc/js/todoModel.js
+++ react-todomvc/js/todoModel.js
@@ -16,71 +16,71 @@ var app = f__useValue((f__setCachedValue(app), f__useValue(f__getCachedValue()))
 	// may not even be worth separating this logic
 	// out, but we do this to demonstrate one way to
 	// separate out parts of your application.
-	app.TodoModel = function (key) {
-		this.key = key;
-		this.todos = Utils.store(key);
-		this.onChanges = [];
-	};
+	f__assign(app, f__StringLiteral('TodoModel'), function (key) {
+		f__assign(this, f__StringLiteral('key'), key);
+		f__assign(this, f__StringLiteral('todos'), Utils.store(key));
+		f__assign(this, f__StringLiteral('onChanges'), []);
+	});
 
-	app.TodoModel.prototype.subscribe = function (onChange) {
+	f__assign(app.TodoModel.prototype, f__StringLiteral('subscribe'), function (onChange) {
 		this.onChanges.push(onChange);
-	};
+	});
 
-	app.TodoModel.prototype.inform = function () {
+	f__assign(app.TodoModel.prototype, f__StringLiteral('inform'), function () {
 		Utils.store(this.key, this.todos);
 		this.onChanges.forEach(function (cb) {
 			cb();
 		});
-	};
+	});
 
-	app.TodoModel.prototype.addTodo = function (title) {
-		this.todos = this.todos.concat(f__makeObject([[f__StringLiteral('id'), Utils.uuid()], [f__StringLiteral('title'), title], [f__StringLiteral('completed'), false]]));
+	f__assign(app.TodoModel.prototype, f__StringLiteral('addTodo'), function (title) {
+		f__assign(this, f__StringLiteral('todos'), this.todos.concat(f__makeObject([['ObjectProperty', f__StringLiteral('id'), Utils.uuid()], ['ObjectProperty', f__StringLiteral('title'), title], ['ObjectProperty', f__StringLiteral('completed'), false]])));
 
 		this.inform();
-	};
+	});
 
-	app.TodoModel.prototype.toggleAll = function (checked) {
+	f__assign(app.TodoModel.prototype, f__StringLiteral('toggleAll'), function (checked) {
 		// Note: it's usually better to use immutable data structures since they're
 		// easier to reason about and React works very well with them. That's why
 		// we use map() and filter() everywhere instead of mutating the array or
 		// todo items themselves.
-		this.todos = this.todos.map(function (todo) {
-			return Utils.extend(f__makeObject([]), todo, f__makeObject([[f__StringLiteral('completed'), checked]]));
-		});
+		f__assign(this, f__StringLiteral('todos'), this.todos.map(function (todo) {
+			return Utils.extend(f__makeObject([]), todo, f__makeObject([['ObjectProperty', f__StringLiteral('completed'), checked]]));
+		}));
 
 		this.inform();
-	};
+	});
 
-	app.TodoModel.prototype.toggle = function (todoToToggle) {
-		this.todos = this.todos.map(function (todo) {
-			return f__useValue(f__notTripleEqual(todo, todoToToggle)) ? todo : Utils.extend(f__makeObject([]), todo, f__makeObject([[f__StringLiteral('completed'), f__not(todo.completed)]]));
-		});
+	f__assign(app.TodoModel.prototype, f__StringLiteral('toggle'), function (todoToToggle) {
+		f__assign(this, f__StringLiteral('todos'), this.todos.map(function (todo) {
+			return f__useValue(f__notTripleEqual(todo, todoToToggle)) ? todo : Utils.extend(f__makeObject([]), todo, f__makeObject([['ObjectProperty', f__StringLiteral('completed'), f__not(todo.completed)]]));
+		}));
 
 		this.inform();
-	};
+	});
 
-	app.TodoModel.prototype.destroy = function (todo) {
-		this.todos = this.todos.filter(function (candidate) {
+	f__assign(app.TodoModel.prototype, f__StringLiteral('destroy'), function (todo) {
+		f__assign(this, f__StringLiteral('todos'), this.todos.filter(function (candidate) {
 			return f__notTripleEqual(candidate, todo);
-		});
+		}));
 
 		this.inform();
-	};
+	});
 
-	app.TodoModel.prototype.save = function (todoToSave, text) {
-		this.todos = this.todos.map(function (todo) {
-			return f__useValue(f__notTripleEqual(todo, todoToSave)) ? todo : Utils.extend(f__makeObject([]), todo, f__makeObject([[f__StringLiteral('title'), text]]));
-		});
+	f__assign(app.TodoModel.prototype, f__StringLiteral('save'), function (todoToSave, text) {
+		f__assign(this, f__StringLiteral('todos'), this.todos.map(function (todo) {
+			return f__useValue(f__notTripleEqual(todo, todoToSave)) ? todo : Utils.extend(f__makeObject([]), todo, f__makeObject([['ObjectProperty', f__StringLiteral('title'), text]]));
+		}));
 
 		this.inform();
-	};
+	});
 
-	app.TodoModel.prototype.clearCompleted = function () {
-		this.todos = this.todos.filter(function (todo) {
+	f__assign(app.TodoModel.prototype, f__StringLiteral('clearCompleted'), function () {
+		f__assign(this, f__StringLiteral('todos'), this.todos.filter(function (todo) {
 			return f__not(todo.completed);
-		});
+		}));
 
 		this.inform();
-	};
+	});
 })();
 //# sourceMappingURL=todoModel.js.map
