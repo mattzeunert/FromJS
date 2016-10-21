16735ccd7f63e888aba8422c86e210eab8478345
Matt Zeunert <matt@mostlystatic.com>
2016-10-21 15:15:27 +0100

    Remove update-website-demos from npm scripts.

---
+++ scripts/publish-website.sh
@@ -0,0 +1,17 @@
+# Pushes gh-pages folder to gh-pages branch
+# Before doing that it includes the generated code in gh-pages in the repo by editing .gitignore
+# After the edit the .gitignore changes are reverted.
+
+STATUS="$(git status)"
+
+if [[ $STATUS == *"nothing to commit, working directory clean"* ]]
+then
+    sed -i "" '/gh-pages/d' ./.gitignore
+    git add .
+    git commit -m "Edit .gitignore to publish"
+    git push origin `git subtree split --prefix gh-pages master`:gh-pages --force
+    git reset HEAD~
+    git checkout .gitignore
+else
+    echo "Need clean working directory to publish"
+fi
