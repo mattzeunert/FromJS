e4529e4dce4b6b99cc3ea84a7ea1b7135962adff
Matt Zeunert <matt@mostlystatic.com>
2016-10-24 15:58:26 +0100

    9999

--- src/e2e/lodash.e2e.js
+++ src/e2e/lodash.e2e.js
@@ -2,7 +2,7 @@ var helpers = require("./helpers")
 
 fdescribe('Lodash Unit tests', function() {
     it("Loads and executes the lodash unit tests", function(){
-        helpers.loadPage('http://localhost:7500/demos/lodash-master/test/index.html')
+        helpers.loadPage('http://localhost:9999/demos/lodash-master/test/index.html')
         .then(helpers.waitForEl("#qunit-testresult .total"))
         .then(function(){
             helpers.getInnerHtml("#qunit-testresult .passed").then(function(html){
