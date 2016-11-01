3d2b157cb8429d23a70ebad0d49ef6fa0cab38d7
Matt Zeunert <matt@mostlystatic.com>
2016-11-01 20:42:11 +0000

    Add test that checks for fromJSInspectElement global.

--- src/e2e/backbone-todomvc.e2e.js
+++ src/e2e/backbone-todomvc.e2e.js
@@ -12,6 +12,14 @@ describe('Backbone TodoMVC', function() {
         .then(helpers.openFromJSInspector)
     })
 
+    it("Exposes a global fromJSInspectElement method", function(){
+        browser.executeScript(function(){
+            return window.fromJSInspectElememt !== undefined
+        }).then(function(hasFromJSInspectElement){
+            expect(hasFromJSInspectElement).toBe(true)
+        })
+    })
+
     it('Correctly traces a checkbox input', function() {
         helpers.inspectElement('input[type="checkbox"]')
         .then(function(){
