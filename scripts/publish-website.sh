7b06e6832f22d2bfd5c25c32eb5e6bd89f8179f8
Matt Zeunert <mattzeunert@Matt-Laptop.local>
2018-03-18 18:57:05 +0800

    safe

--- scripts/publish-website.sh
+++ scripts/publish-website.sh
@@ -9,7 +9,7 @@ then
     sed -i "" '/gh-pages/d' ./.gitignore
     git add .
     git commit -m "Edit .gitignore to publish"
-    git push origin `git subtree split --prefix gh-pages master`:gh-pages --force
+    git push origin `git subtree split --prefix gh-pages v2`:gh-pages --force
     git reset HEAD~
     git checkout .gitignore
 else
