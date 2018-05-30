58fef9e4b34053066dda447fe9ba1a0ca8d16100
Matt Zeunert <matt@mostlystatic.com>
2018-05-30 21:47:42 +0100

    Make launching CLI less annoying

--- packages/cli/cli.ts
+++ packages/cli/cli.ts
@@ -1,23 +1,36 @@
 import * as commander from "commander";
 import Backend from "@fromjs/backend";
 import * as puppeteer from "puppeteer";
-import * as opn from "opn";
+import * as process from "process";
 
-commander.version(require("../package.json").version).parse(process.argv);
+commander
+  .option("--shouldOpenBrowser <shouldOpen>", "yes|no|only", "yes")
+  .version(require("../package.json").version)
+  .parse(process.argv);
 
 let bePort = 7000;
 let proxyPort = bePort + 1;
 
-const backend = new Backend({
-  bePort,
-  proxyPort,
-  onReady: async function() {
-    opn("http://localhost:" + bePort);
-    const browser = await puppeteer.launch({
-      args: ["--proxy-server=127.0.0.1:" + proxyPort],
-      headless: false
-    });
-    const page = await browser.newPage();
-    await page.goto("http://localhost:" + bePort + "/start");
-  }
-});
+if (commander.shouldOpenBrowser === "only") {
+  console.log("Only opening browser with proxy port set to", proxyPort);
+  openBrowser();
+} else {
+  const backend = new Backend({
+    bePort,
+    proxyPort,
+    onReady: async function() {
+      if (commander.shouldOpenBrowser === "yes") {
+        openBrowser();
+      }
+    }
+  });
+}
+
+async function openBrowser() {
+  const browser = await puppeteer.launch({
+    args: ["--proxy-server=127.0.0.1:" + proxyPort],
+    headless: false
+  });
+  const page = await browser.newPage();
+  await page.goto("http://localhost:" + bePort + "/start");
+}
