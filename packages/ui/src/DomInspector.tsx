abbefac647734d18ed5586d50cf9b000549923f7
Matt Zeunert <matt@mostlystatic.com>
2018-06-13 16:12:24 +0100

    Collapse dom inspector if directly selecting char elsewhere

--- packages/ui/src/DomInspector.tsx
+++ packages/ui/src/DomInspector.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { branch, root } from "baobab-react/higher-order";
 import appState from "./appState";
 import { callApi, inspectDomChar } from "./api";
-import { selectInspectedDomCharIndex } from "./actions";
+import { selectInspectedDomCharIndex, expandDomInspector } from "./actions";
 import { TextEl } from "./TextEl";
 
 let DomInspector = class DomInspector extends React.Component<any, any> {
@@ -12,23 +12,32 @@ let DomInspector = class DomInspector extends React.Component<any, any> {
     }
     return (
       <div>
-        <div className="title">Inspected DOM HTML</div>
-        <div style={{ border: "1px solid #ddd", marginBottom: 10 }}>
-          <TextEl
-            highlightedCharacterIndex={this.props.domToInspect.charIndex}
-            onCharacterClick={charIndex => {
-              selectInspectedDomCharIndex(charIndex);
-            }}
-            text={this.props.domToInspect.outerHTML}
-          />
+        <div className="title">
+          Inspected DOM HTML{this.props.collapseDomInspector && (
+            <button onClick={() => expandDomInspector()}>
+              Show inspected Element
+            </button>
+          )}
         </div>
+        {!this.props.collapseDomInspector && (
+          <div style={{ border: "1px solid #ddd", marginBottom: 10 }}>
+            <TextEl
+              highlightedCharacterIndex={this.props.domToInspect.charIndex}
+              onCharacterClick={charIndex => {
+                selectInspectedDomCharIndex(charIndex);
+              }}
+              text={this.props.domToInspect.outerHTML}
+            />
+          </div>
+        )}
       </div>
     );
   }
 };
 DomInspector = branch(
   {
-    domToInspect: ["domToInspect"]
+    domToInspect: ["domToInspect"],
+    collapseDomInspector: ["collapseDomInspector"]
   },
   DomInspector
 );
--- packages/ui/src/appState.tsx
+++ packages/ui/src/appState.tsx
@@ -4,26 +4,9 @@ import { loadSteps } from "./api";
 var appState = new Baobab({
   debugMode: false,
   steps: [],
-  inspectionTarget: null
+  inspectionTarget: null,
+  collapseDomInspector: false
 });
 window["appState"] = appState;
 
 export default appState;
-
-// maybe create effect.ts file for .on stuff?
-
-appState.select("inspectionTarget").on("update", ({ target }) => {
-  const inspectionTarget = target.get();
-  if (!inspectionTarget || !inspectionTarget.logId) {
-    console.log("no inspection target!!");
-  } else {
-    console.time("Load Steps");
-    loadSteps(inspectionTarget).then(r => {
-      console.timeEnd("Load Steps");
-      var steps = r.steps;
-      console.log({ steps });
-
-      appState.set("steps", steps);
-    });
-  }
-});
