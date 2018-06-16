c4785ab3a18ffdf373710d68c9f65ad12e0c88c2
Matt Zeunert <matt@mostlystatic.com>
2018-06-16 23:56:16 +0100

    Tweak test code

--- packages/cli/e2e.test.ts
+++ packages/cli/e2e.test.ts
@@ -20,35 +20,37 @@ function waitForProxyReady(command) {
 }
 
 describe("E2E", () => {
+  let browser;
+
   beforeAll(async () => {
     await killPort(12100);
     await killPort(12101);
+
+    command = spawn(__dirname + "/bin/fromjs", [
+      "--port",
+      "12100",
+      "--shouldOpenBrowser",
+      "no",
+      "--sessionDirectory",
+      "/tmp/fromjs-e2e"
+    ]);
+
+    command.stderr.on("data", function(data) {
+      console.log("err", data.toString());
+    });
+
+    browser = await puppeteer.launch({
+      args: ["--proxy-server=127.0.0.1:12101"],
+      headless: true
+    });
+
+    await waitForProxyReady(command);
   });
 
   let command;
   it(
     "works",
     async () => {
-      console.time("e2e");
-      command = spawn(__dirname + "/bin/fromjs", [
-        "--port",
-        "12100",
-        "--shouldOpenBrowser",
-        "no",
-        "--sessionDirectory",
-        "/tmp/fromjs-e2e"
-      ]);
-
-      command.stderr.on("data", function(data) {
-        console.log("err", data.toString());
-      });
-
-      await waitForProxyReady(command);
-
-      const browser = await puppeteer.launch({
-        args: ["--proxy-server=127.0.0.1:12101"],
-        headless: true
-      });
       const page = await browser.newPage();
       await page.goto("http://localhost:12100");
       await page.waitForSelector(".step");
