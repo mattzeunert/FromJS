61ea891b2537c03d35e76a5c62f15301f3c04852
Matt Zeunert <matt@mostlystatic.com>
2018-06-18 15:47:48 +0100

    Track DOM creation with createComment

--- packages/cli/test/app.js
+++ packages/cli/test/app.js
@@ -1,25 +1,28 @@
+const app = document.querySelector("#app");
+
 const span = document.createElement("span");
 span.setAttribute("attr", "setAttribute");
 span.innerHTML = "abc<b>innerHTML</b>";
+app.appendChild(span);
 
 const div = document.createElement("div");
 div.textContent = "textContent";
+app.appendChild(div);
 
 const div2 = document.createElement("div");
 div2.innerHTML = "<!-- COMMENT_IN_INNERTHML -->";
+app.appendChild(div2);
+
+const comment = document.createComment("createComment");
+app.appendChild(comment);
 
 const textNode = document.createTextNode("createTextNode");
+app.appendChild(textNode);
 
-const app = document.querySelector("#app");
-
-app.appendChild(span);
-app.appendChild(div);
-app.appendChild(div2);
 app.insertAdjacentHTML(
   "afterbegin",
   "<div>insertAdjacentHTML1</div><div>insertAdjacentHTML2</div>"
 );
-app.appendChild(textNode);
 
 fromJSInspect(app);
 
