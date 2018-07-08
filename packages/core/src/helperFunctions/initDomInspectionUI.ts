5a5ea9dbe843b4f580650ec5de1570ab7ae75f92
Matt Zeunert <matt@mostlystatic.com>
2018-07-08 12:41:37 +0100

    Tweak element selection UX

--- packages/core/src/helperFunctions/initDomInspectionUI.ts
+++ packages/core/src/helperFunctions/initDomInspectionUI.ts
@@ -47,8 +47,9 @@ export default function initDomInspectionUI() {
     const rect = element.getBoundingClientRect();
     let marker = getMarkerElementFromHighlightReason(highlightReason);
 
-    const color = highlightReason === "selected" ? "blue" : "green";
+    const color = highlightReason === "selected" ? "#236fb1" : "#3a94e2";
     const zIndex = highlightReason === "selected" ? 10000001 : 10000000;
+    const boxTop = rect.top + document.body.scrollTop; // note: this won't work well if inside another scrollable element
     marker.setAttribute(
       "style",
       "position: absolute; display: block; z-index: " +
@@ -59,12 +60,12 @@ export default function initDomInspectionUI() {
         "left: " +
         rect.left +
         "px;top: " +
-        rect.top +
+        boxTop +
         "px;height: " +
         rect.height +
         "px;width: " +
         rect.width +
-        "px;"
+        "px; background: rgba(35, 111, 177, .1);"
     );
   }
 
