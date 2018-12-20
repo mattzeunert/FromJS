1ba8b11c8ea391eb49bd347f48284ba1c1498754
Matt Zeunert <matt@mostlystatic.com>
2018-12-20 09:58:23 +0000

    Fail on new node version

--- packages/cli/cli.ts
+++ packages/cli/cli.ts
@@ -6,6 +6,10 @@ import * as puppeteer from "puppeteer";
 
 const list = val => val.split(",");
 
+if (process.version.startsWith("v11") || process.version.startsWith("v10")) {
+  throw Error("Newer node version not support, please use node 8")
+}
+
 const maxOldSpaceSizeArg = process.execArgv.find(arg =>
   arg.includes("--max_old_space_size")
 );
