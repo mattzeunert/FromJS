46dadd721153dd65296e1bc4627c119d1d98203d
Matt Zeunert <matt@mostlystatic.com>
2018-12-23 12:10:30 +0000

    Traverse Number call

--- packages/core/src/operations/CallExpressionSpecialCases.ts
+++ packages/core/src/operations/CallExpressionSpecialCases.ts
@@ -1060,6 +1060,11 @@ export function traverseKnownFunction({
         operationLog: operationLog.args.context,
         charIndex: charIndex
       };
+    case "Number":
+      return {
+        operationLog: operationLog.args.arg0,
+        charIndex
+      };
     case "Math.round":
       return {
         operationLog: operationLog.args.arg0,
