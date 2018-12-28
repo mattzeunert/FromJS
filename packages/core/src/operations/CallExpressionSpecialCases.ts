78aad7457919385a45603be0d74235bda7b4904a
Matt Zeunert <matt@mostlystatic.com>
2018-12-28 19:52:15 +0000

    Traverse Math.abs

--- packages/core/src/operations/CallExpressionSpecialCases.ts
+++ packages/core/src/operations/CallExpressionSpecialCases.ts
@@ -1217,6 +1217,12 @@ export function traverseKnownFunction({
         operationLog: operationLog.args.arg0,
         charIndex
       };
+
+    case "Math.abs":
+      return {
+        operationLog: operationLog.args.arg0,
+        charIndex
+      };
     default:
       return {
         operationLog: operationLog.extraArgs.returnValue,
