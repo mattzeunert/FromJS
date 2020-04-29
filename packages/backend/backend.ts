430b34f83028a8fd2504c17591212ca03cc6bf43
Matt Zeunert <matt@mostlystatic.com>
2020-04-29 16:26:51 +0100

    dsf

--- packages/backend/backend.ts
+++ packages/backend/backend.ts
@@ -184,6 +184,7 @@ async function compileNodeApp(baseDirectory, requestHandler: RequestHandler) {
 
 async function generateLocLogs({ logServer, locLogs }) {
   let id = "generateLocLogs";
+  console.log("will generate locLogs");
   await new Promise((resolve) => locLogs._db.clear(resolve));
   console.time(id);
 
@@ -327,20 +328,20 @@ export default class Backend {
           )
         )
       : {};
-    // setInterval(() => {
-    //   console.time("save json");
-    //   fs.writeFileSync(
-    //     options.sessionDirectory + "/files.json",
-    //     JSON.stringify(files, null, 2)
-    //   );
-    //   if (ENABLE_DERIVED) {
-    //     fs.writeFileSync(
-    //       options.sessionDirectory + "/logUses.json",
-    //       JSON.stringify(logUses, null, 2)
-    //     );
-    //   }
-    //   console.timeEnd("save json");
-    // }, 10000);
+    setInterval(() => {
+      console.time("save json");
+      fs.writeFileSync(
+        options.sessionDirectory + "/files.json",
+        JSON.stringify(files, null, 2)
+      );
+      // if (ENABLE_DERIVED) {
+      //   fs.writeFileSync(
+      //     options.sessionDirectory + "/logUses.json",
+      //     JSON.stringify(logUses, null, 2)
+      //   );
+      // }
+      console.timeEnd("save json");
+    }, 10000);
 
     let requestHandler;
 
