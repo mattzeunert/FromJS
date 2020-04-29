96bfb5ac436c55f76fdcd59fd21223a7df429e2a
Matt Zeunert <matt@mostlystatic.com>
2020-04-29 15:42:43 +0100

    lh

--- node-test/test.js
+++ node-test/test.js
@@ -8,59 +8,68 @@
 
 
 
-const prettyBytes = require("pretty-bytes")
+// const prettyBytes = require("pretty-bytes")
 
-let bytes = 400000
-let msg = "Size: " + prettyBytes(bytes)
-console.log(msg)
+// let bytes = 400000
+// let msg = "Size: " + prettyBytes(bytes)
+// console.log(msg)
 
-setTimeout(() => {
-    console.log("waited 10s")
-}, 10000)
+// setTimeout(() => {
+//     console.log("waited 10s")
+// }, 10000)
 
-// const lighthouse = require('lighthouse');
-// const chromeLauncher = require('chrome-launcher');
-// const log = require("lighthouse-logger");
-// const saveResults = require("./node_modules/lighthouse/lighthouse-cli/run").saveResults
-// log.setLevel("info");
+const lighthouse = require('lighthouse');
+const chromeLauncher = require('chrome-launcher');
+const log = require("lighthouse-logger");
+const saveResults = require("./node_modules/lighthouse/lighthouse-cli/run").saveResults
+log.setLevel("info");
 
-// function launchChromeAndRunLighthouse(url, opts, config = null) {
-//     // return chromeLauncher.launch({ chromeFlags: opts.chromeFlags }).then(chrome => {
-//     //     opts.port = chrome.port;
-//     return lighthouse(url, opts, config).then(results => {
-//         // use results.lhr for the JS-consumable output
-//         // https://github.com/GoogleChrome/lighthouse/blob/master/types/lhr.d.ts
-//         // use results.report for the HTML/JSON/CSV output as a string
-//         // use results.artifacts for the trace/screenshots/other specific case you need (rarer)
-//         return results
-//         // return chrome.kill().then(() => results.lhr)
-//     });
-//     // });
-// }
+function launchChromeAndRunLighthouse(url, opts, config = null) {
+    return chromeLauncher.launch({ chromeFlags: opts.chromeFlags }).then(chrome => {
+        opts.port = chrome.port;
+        return lighthouse(url, opts, config).then(results => {
+            // use results.lhr for the JS-consumable output
+            // https://github.com/GoogleChrome/lighthouse/blob/master/types/lhr.d.ts
+            // use results.report for the HTML/JSON/CSV output as a string
+            // use results.artifacts for the trace/screenshots/other specific case you need (rarer)
+            return results
+            // return chrome.kill().then(() => results.lhr)
+        });
+    });
+}
 
-// const opts = {
-//     onlyCategories: ['seo'],
-//     // gatherMode: "../lh-artifacts",
-//     auditMode: "../lh-artifacts",
-//     logLevel: "info"
-//     // chromeFlags: ['--show-paint-rects']
-// };
+const opts = {
+    onlyCategories: ['seo'],
 
-// // Usage:
-// launchChromeAndRunLighthouse('https://example.com', opts).then(runnerResult => {
-//     saveResults(runnerResult, { output: "html" }).then(() => {
-//         console.log("done save")
-//         process.exit()
-//     })
+    logLevel: "info"
+    // chromeFlags: ['--show-paint-rects']
+};
 
-//     // const rep = results.report
-//     // console.log(rep.slice(0, 100))
-//     // const str = JSON.stringify(results)
-//     // console.log(Object.keys(results), str.slice(0, 1000))
+console.log(process.argv)
+if (process.argv[2] === "gather") {
+    opts.gatherMode = "../lh-artifacts"
+} else {
+    opts.auditMode = "../lh-artifacts"
+}
 
-//     // Use results!
-// });
+// Usage:
+launchChromeAndRunLighthouse('https://example.com', opts).then(runnerResult => {
+    saveResults(runnerResult, { output: "html" }).then(() => {
+        console.log("done save")
+        setTimeout(() => {
+            console.log("Will exit")
+            process.exit()
+        }, 20 * 1000)
+    })
+
+    // const rep = results.report
+    // console.log(rep.slice(0, 100))
+    // const str = JSON.stringify(results)
+    // console.log(Object.keys(results), str.slice(0, 1000))
+
+    // Use results!
+});
 
 
-// // const lighthouse = require("lighthouse")
+// const lighthouse = require("lighthouse")
 
