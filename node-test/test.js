34aa32b9e8e81631ee43d5a7f41954a3e0a84e72
Matt Zeunert <matt@mostlystatic.com>
2020-04-30 11:33:16 +0100

    Fix run lh

--- node-test/test.js
+++ node-test/test.js
@@ -1,3 +1,72 @@
+const lighthouse = require('lighthouse');
+const chromeLauncher = require('chrome-launcher');
+const log = require("lighthouse-logger");
+const saveResults = require("./node_modules/lighthouse/lighthouse-cli/run").saveResults
+log.setLevel("info");
+
+function launchChromeAndRunLighthouse(url, opts, config = null) {
+    let chromePromise = Promise.resolve({})
+    if (isGatherMode) {
+        chromePromise = chromeLauncher.launch({ chromeFlags: opts.chromeFlags })
+    }
+    return chromePromise.then(chrome => {
+        opts.port = chrome.port;
+        debugger
+        return lighthouse(url, opts, config).then(results => {
+
+            console.log(typeof results.report)
+            // use results.lhr for the JS-consumable output
+            // https://github.com/GoogleChrome/lighthouse/blob/master/types/lhr.d.ts
+            // use results.report for the HTML/JSON/CSV output as a string
+            // use results.artifacts for the trace/screenshots/other specific case you need (rarer)
+            return results
+            // return chrome.kill().then(() => results.lhr)
+        });
+    });
+}
+
+const opts = {
+    onlyCategories: ['seo'],
+    output: ["json", "html"],
+    logLevel: "info"
+    // chromeFlags: ['--show-paint-rects']
+};
+
+console.log(process.argv)
+let isGatherMode = process.argv[2] === "gather"
+if (isGatherMode) {
+    opts.gatherMode = "../lh-artifacts"
+} else {
+    opts.auditMode = "../lh-artifacts"
+}
+
+// Usage:
+launchChromeAndRunLighthouse('https://example.com', opts).then(runnerResult => {
+    // const reportStr = JSON.stringify(runnerResult.report)
+    // runnerResult.report = JSON.parse(runnerResult.report)
+    // console.log(runnerResult.report)
+    console.log(typeof runnerResult.report)
+    saveResults(runnerResult, { output: ["json", "html"] }).then(() => {
+        console.log("done save, will wait 20s then exit")
+        setTimeout(() => {
+            console.log("Will exit")
+            process.exit()
+        }, 20 * 1000)
+    })
+
+    // const rep = results.report
+    // console.log(rep.slice(0, 100))
+    // const str = JSON.stringify(results)
+    // console.log(Object.keys(results), str.slice(0, 1000))
+
+    // Use results!
+});
+
+
+// const lighthouse = require("lighthouse")
+
+
+
 // const prettier = require("prettier");
 // const axios = require("axios")
 
@@ -18,59 +87,4 @@
 //     console.log("waited 10s")
 // }, 10000)
 
-const lighthouse = require('lighthouse');
-const chromeLauncher = require('chrome-launcher');
-const log = require("lighthouse-logger");
-const saveResults = require("./node_modules/lighthouse/lighthouse-cli/run").saveResults
-log.setLevel("info");
-
-function launchChromeAndRunLighthouse(url, opts, config = null) {
-    return chromeLauncher.launch({ chromeFlags: opts.chromeFlags }).then(chrome => {
-        opts.port = chrome.port;
-        return lighthouse(url, opts, config).then(results => {
-            // use results.lhr for the JS-consumable output
-            // https://github.com/GoogleChrome/lighthouse/blob/master/types/lhr.d.ts
-            // use results.report for the HTML/JSON/CSV output as a string
-            // use results.artifacts for the trace/screenshots/other specific case you need (rarer)
-            return results
-            // return chrome.kill().then(() => results.lhr)
-        });
-    });
-}
-
-const opts = {
-    onlyCategories: ['seo'],
-
-    logLevel: "info"
-    // chromeFlags: ['--show-paint-rects']
-};
-
-console.log(process.argv)
-if (process.argv[2] === "gather") {
-    opts.gatherMode = "../lh-artifacts"
-} else {
-    opts.auditMode = "../lh-artifacts"
-}
-
-// Usage:
-launchChromeAndRunLighthouse('https://example.com', opts).then(runnerResult => {
-    const reportStr = JSON.stringify(runnerResult.report)
-    saveResults(runnerResult, { output: ["html"] }).then(() => {
-        console.log("done save")
-        setTimeout(() => {
-            console.log("Will exit")
-            process.exit()
-        }, 20 * 1000)
-    })
-
-    // const rep = results.report
-    // console.log(rep.slice(0, 100))
-    // const str = JSON.stringify(results)
-    // console.log(Object.keys(results), str.slice(0, 1000))
-
-    // Use results!
-});
-
-
-// const lighthouse = require("lighthouse")
 
