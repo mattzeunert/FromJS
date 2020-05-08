b537eee9ca5604a06d8b2cc9932ce9d3b94c4200
Matt Zeunert <matt@mostlystatic.com>
2020-05-08 12:23:12 +0100

    n=5

--- packages/backend/node.test.ts
+++ packages/backend/node.test.ts
@@ -93,8 +93,7 @@ describe("Node", () => {
     let { step, execDuration, compileDuration } = await runTest("reactSsr", {
       charIndex: 0,
       ignoreFilePattern: /umd|profiling|production|unstable|test\-utils|scheduler\-tracing|envify|browser|\.min\.js|factoryWithTypeCheckers/,
-
-      // runNTimes: 5,
+      runNTimes: 5,
     });
     console.log({ execDuration, compileDuration });
 
